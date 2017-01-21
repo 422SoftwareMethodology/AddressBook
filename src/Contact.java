1eb75638f520f86bba2cc831d70183ffa65f9699
Craig Totten <craigt@uoregon.edu>
2017-01-21 14:51:00 -0800

    more changes to contact

--- src/Contact.java
+++ src/Contact.java
@@ -1,5 +1,6 @@
+import java.util.Comparator;
 
-public class Contact {
+public class Contact implements Comparator<Contact>, Comparable<Contact> {
 	private String _firstName;
 	private String _lastName;
 	private String _phoneNumber;
@@ -16,63 +17,73 @@ public class Contact {
 		return _firstName;
 	}
 
-	public void set_firstName(String _firstName) {
-		this._firstName = _firstName;
+	public void set_firstName(String firstName) {
+		this._firstName = firstName;
 	}
 
 	public String get_lastName() {
 		return _lastName;
 	}
 
-	public void set_lastName(String _lastName) {
-		this._lastName = _lastName;
+	public void set_lastName(String lastName) {
+		this._lastName = lastName;
 	}
 
 	public String get_phoneNumber() {
 		return _phoneNumber;
 	}
 
-	public void set_phoneNumber(String _phoneNumber) {
-		this._phoneNumber = _phoneNumber;
+	public void set_phoneNumber(String phoneNumber) {
+		this._phoneNumber = phoneNumber;
 	}
 
 	public String get_address() {
 		return _address;
 	}
 
-	public void set_address(String _address) {
-		this._address = _address;
+	public void set_address(String address) {
+		this._address = address;
 	}
 
 	public String get_city() {
 		return _city;
 	}
 
-	public void set_city(String _city) {
-		this._city = _city;
+	public void set_city(String city) {
+		this._city = city;
 	}
 
 	public String get_state() {
 		return _state;
 	}
 
-	public void set_state(String _state) {
-		this._state = _state;
+	public void set_state(String state) {
+		this._state = state;
 	}
 
 	public String get_email() {
 		return _email;
 	}
 
-	public void set_email(String _email) {
-		this._email = _email;
+	public void set_email(String email) {
+		this._email = email;
 	}
 
 	public int get_zip() {
 		return _zip;
 	}
 
-	public void set_zip(int _zip) {
-		this._zip = _zip;
+	public void set_zip(int zip) {
+		this._zip = zip;
+	}
+
+	@Override
+	public int compareTo(Contact c) {
+		return (this._lastName).compareTo(c._lastName);
+	}
+
+	@Override
+	public int compare(Contact c1, Contact c2) {
+		return (c1._zip - c2._zip);
 	}
 }
