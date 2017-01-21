a0231533f36d6cf6366373d40a065b9d0a8886b4
Craig Totten <craigt@uoregon.edu>
2017-01-21 14:23:24 -0800

    Updated Contact

--- src/Contact.java
+++ src/Contact.java
@@ -1,5 +1,78 @@
-package addressbook;
 
 public class Contact {
+	private String _firstName;
+	private String _lastName;
+	private String _phoneNumber;
+	private String _address;
+	private String _city;
+	private String _state;
+	private String _email;
+	private int _zip;
+	
+	public Contact () {
+	}
 
+	public String get_firstName() {
+		return _firstName;
+	}
+
+	public void set_firstName(String _firstName) {
+		this._firstName = _firstName;
+	}
+
+	public String get_lastName() {
+		return _lastName;
+	}
+
+	public void set_lastName(String _lastName) {
+		this._lastName = _lastName;
+	}
+
+	public String get_phoneNumber() {
+		return _phoneNumber;
+	}
+
+	public void set_phoneNumber(String _phoneNumber) {
+		this._phoneNumber = _phoneNumber;
+	}
+
+	public String get_address() {
+		return _address;
+	}
+
+	public void set_address(String _address) {
+		this._address = _address;
+	}
+
+	public String get_city() {
+		return _city;
+	}
+
+	public void set_city(String _city) {
+		this._city = _city;
+	}
+
+	public String get_state() {
+		return _state;
+	}
+
+	public void set_state(String _state) {
+		this._state = _state;
+	}
+
+	public String get_email() {
+		return _email;
+	}
+
+	public void set_email(String _email) {
+		this._email = _email;
+	}
+
+	public int get_zip() {
+		return _zip;
+	}
+
+	public void set_zip(int _zip) {
+		this._zip = _zip;
+	}
 }
