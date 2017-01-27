7490765f0576c73d198319300bfc96911a33e846
zhenqin0630 <zqin@uoregon.edu>
2017-01-26 17:47:05 -0800

    Add files via upload

--- src/PersonInfo.java
+++ src/PersonInfo.java
@@ -15,50 +15,59 @@ import javax.swing.JTextField;
 @SuppressWarnings("serial")
 public class PersonInfo extends JFrame{
 	private JPanel InfoPanel;
-	private JLabel FirstName, LastName, phonenumber, ZIP_code, city, state, address, email;
-	private JTextField fname, lname, phonenum, zip, c, s, a, e;
+	private JLabel FirstName, LastName, phonenumber, ZIP_code, city, state, address1, address2, email;
+	private JTextField fname, lname, phonenum, zip, c, s, a1, a2, e;
 	private JButton Done, Cancel;
-	private String inputfname;
 	private Contact tempContact;
 	public PersonInfo(ArrayList<Contact> openContactList){
 		super("Contact Info!");
 		tempContact = new Contact(" ", " ", " ", " ", " ", " ", " ", " ");
 		setLayout(new BorderLayout());
 		
-		InfoPanel = new JPanel(new GridLayout(9, 2));      //one panel in this interface
+		InfoPanel = new JPanel(new GridLayout(10, 2));      //one panel in this interface
 		
 		FirstName = new JLabel("            FirstName");     //all attributes labels
 		LastName = new JLabel("            LastName");
 		phonenumber = new JLabel("            Phonenumber");
-		address = new JLabel("            Address");
+		address1 = new JLabel("            Address1");
+		address2 = new JLabel("            Address2");
 		city = new JLabel("            City");
 		state = new JLabel("            State");
 		ZIP_code = new JLabel("            ZIP");
 		email = new JLabel("            Email address");
 		
 		fname = new JTextField();  //all text areas
-		inputfname = fname.getText();
 		lname = new JTextField();
 		phonenum = new JTextField();
 		zip = new JTextField();
 		c = new JTextField();
 		s = new JTextField();
-		a = new JTextField();
+		a1 = new JTextField();
+		a2 = new JTextField();
 		e = new JTextField();
 		
 		Done = new JButton("Done!");    //click this button to save the info
 		Done.addActionListener(new ActionListener(){  
 			public void actionPerformed(ActionEvent e) { 
-				tempContact.set_firstName(fname.getText());
-				tempContact.set_lastName(lname.getText());
-				tempContact.set_phoneNumber(phonenum.getText());
-				tempContact.set_address(a.getText());
-				tempContact.set_city(c.getText());
-				tempContact.set_state(s.getText());
-				tempContact.set_zip(zip.getText());
-				openContactList.add(tempContact);
-				Frame1.AddContactToTable();
-				dispose();                                //after customer click the button, close the current window and save data
+				if(fname.getText().isEmpty() && lname.getText().isEmpty()){
+					prompt1 p1 = new prompt1();
+					p1.setLocation(300, 200);
+				}
+				else{
+					tempContact.set_firstName(fname.getText());
+					tempContact.set_lastName(lname.getText());
+					tempContact.set_phoneNumber(phonenum.getText());
+					tempContact.set_address(a1.getText());
+					tempContact.set_address(a2.getText());
+					tempContact.set_city(c.getText());
+					tempContact.set_state(s.getText());
+					tempContact.set_zip(zip.getText());
+					openContactList.add(tempContact);
+					Frame1.AddContactToTable();
+					dispose();                                //after customer click the button, close the current window and save data
+				}
+				
+				
         }   
     });  
 		Cancel = new JButton("Cancel");
@@ -74,8 +83,10 @@ public class PersonInfo extends JFrame{
 		InfoPanel.add(lname);
 		InfoPanel.add(phonenumber);
 		InfoPanel.add(phonenum);
-		InfoPanel.add(address);
-		InfoPanel.add(a);
+		InfoPanel.add(address1);
+		InfoPanel.add(a1);
+		InfoPanel.add(address2);
+		InfoPanel.add(a2);
 		InfoPanel.add(city);
 		InfoPanel.add(c);
 		InfoPanel.add(state);
@@ -93,5 +104,14 @@ public class PersonInfo extends JFrame{
 		setSize(500,500);         //set frame size
 		setVisible(true);
 		
+
+		
 	}
+	/*public boolean hasblank(String s){
+		int i = s.indexOf(" ");
+		if(i >= 0)
+			return true;
+		else
+			return false;
+		}*/
 }
