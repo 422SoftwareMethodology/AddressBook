561e44a410c791d732734d31ee4d8c3ad1fbdc24
Alex Costello <costello@uoregon.edu>
2017-02-01 21:09:51 -0800

    changed reader

--- src/Reader.java
+++ src/Reader.java
@@ -12,21 +12,19 @@ public class Reader {
 		String tsvFile = fileLocation + ".tsv";
 		String line = "";
 		String tsvSplitBy = "\t";
-
+		
 		ArrayList<Contact> tempContactList = new ArrayList<>();
 
 		try (BufferedReader br = new BufferedReader(new FileReader(tsvFile))) {
-
+			br.readLine();
 			while ((line = br.readLine()) != null) {
 				// contact array of a single person, use comma to delimit
-				if (line.equals("\n")) {
-					continue;
-				} else {
 					String[] infoArr = line.split(tsvSplitBy);
 					String[] contactInfo = { "", "", "", "", "", "", "", "", "", "" };
 					System.out.println(Integer.toString(infoArr.length));
 					for (int i = 0; i < infoArr.length; i++) {
 						contactInfo[i] = infoArr[i];
+						System.out.println(infoArr[i]);
 					}
 					if (infoArr.length <= 8) {
 						System.out.println("DOINGIT");
@@ -35,6 +33,7 @@ public class Reader {
 								" ");
 						tempContactList.add(tempContact);
 						System.out.println("Contact: firstName = " + tempContact.get_firstName());
+						
 					} else {
 						Contact tempContact = new Contact(infoArr[0], infoArr[1], infoArr[2], infoArr[3], infoArr[4],
 								infoArr[5], infoArr[6], infoArr[7], infoArr[8], infoArr[9]);
@@ -48,7 +47,7 @@ public class Reader {
 					 * contact[4] + " , email=" + contact[5] ); //For Debugging
 					 */
 
-				}
+				
 			}
 		} catch (IOException e) {
 			// e.printStackTrace();
