0c59ce0dfcfd7b425e33042951b210539feefad3
Craig Totten <craigt@uoregon.edu>
2017-02-01 21:16:43 -0800

    Reader conforms to proper format

--- src/Writer.java
+++ src/Writer.java
@@ -49,21 +49,21 @@ public class Writer {
 
 			for (int i = 0; i < arrayListSize; i++) {
 				Contact focusContact = contactArrayList.get(i);
+				sb.append(focusContact.get_city());
+				sb.append('\t');
+				sb.append(focusContact.get_state());
+				sb.append('\t');
+				sb.append(focusContact.get_zip());
+				sb.append('\t');
+				sb.append(focusContact.get_address());
+				sb.append('\t');
+				sb.append(focusContact.get_address2());
+				sb.append('\t');
+				sb.append(focusContact.get_lastName());
+				sb.append('\t');
 				sb.append(focusContact.get_firstName());
 				sb.append('\t');
-				sb.append(focusContact.get_lastName()); // LastName 1
-				sb.append('\t');
-				sb.append(focusContact.get_phoneNumber()); // PhoneNumber 2
-				sb.append('\t');
-				sb.append(focusContact.get_address()); // Address 3
-				sb.append('\t');
-				sb.append(focusContact.get_address()); // Address2 4
-				sb.append('\t');
-				sb.append(focusContact.get_city()); // City 5
-				sb.append('\t');
-				sb.append(focusContact.get_state()); // State 6
-				sb.append('\t');
-				sb.append(focusContact.get_zip()); // zip 7
+				sb.append(focusContact.get_phoneNumber());
 				sb.append('\t');
 				sb.append(focusContact.get_email()); // email 8
 				sb.append('\t');
@@ -87,22 +87,22 @@ public class Writer {
 
 			for (int i = 0; i < arrayListSize; i++) {
 				Contact focusContact = contactArrayList.get(i);
+				sb.append(focusContact.get_city());
+				sb.append('\t');
+				sb.append(focusContact.get_state());
+				sb.append('\t');
+				sb.append(focusContact.get_zip());
+				sb.append('\t');
+				sb.append(focusContact.get_address());
+				sb.append('\t');
+				sb.append(focusContact.get_address2());
+				sb.append('\t');
+				sb.append(focusContact.get_lastName());
+				sb.append('\t');
 				sb.append(focusContact.get_firstName());
 				sb.append('\t');
-				sb.append(focusContact.get_lastName()); // LastName 1
-				sb.append('\t');
-				sb.append(focusContact.get_phoneNumber()); // PhoneNumber 2
-				sb.append('\t');
-				sb.append(focusContact.get_address()); // Address 3
-				sb.append('\t');
-				sb.append(focusContact.get_address()); // Address2 4
-				sb.append('\t');
-				sb.append(focusContact.get_city()); // City 5
-				sb.append('\t');
-				sb.append(focusContact.get_state()); // State 6
-				sb.append('\t');
-				sb.append(focusContact.get_zip()); // zip 7
-				sb.append('\n'); // end of contact
+				sb.append(focusContact.get_phoneNumber());
+				sb.append('\n');
 			}
 
 			pw.write(sb.toString());
