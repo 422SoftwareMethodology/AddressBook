9d476fd691810dd4feaa9d61f42a56e82baf9eaa
Alex Costello <costello@uoregon.edu>
2017-02-01 21:33:04 -0800

    ignore invalid entries

--- src/Reader.java
+++ src/Reader.java
@@ -20,32 +20,36 @@ public class Reader {
 			while ((line = br.readLine()) != null) {
 				// contact array of a single person, use comma to delimit
 					String[] infoArr = line.split(tsvSplitBy);
-					String[] contactInfo = { "", "", "", "", "", "", "", "", "", "" };
+					String[] contactInfo = { " ", " ", " ", " ", " ", " ", " ", " ", " ", " " };
 					System.out.println(Integer.toString(infoArr.length));
 					for (int i = 0; i < infoArr.length; i++) {
 						contactInfo[i] = infoArr[i];
 						System.out.println(infoArr[i]);
 					}
-					if (infoArr.length <= 8) {
-						System.out.println("DOINGIT");
-						Contact tempContact = new Contact(contactInfo[0], contactInfo[1], contactInfo[2],
+					if(!isValid(contactInfo)){
+						continue;
+					}else{
+						if (infoArr.length <= 8) {
+							System.out.println("DOINGIT");
+							Contact tempContact = new Contact(contactInfo[0], contactInfo[1], contactInfo[2],
 								contactInfo[3], contactInfo[4], contactInfo[5], contactInfo[6], contactInfo[7], " ",
 								" ");
-						tempContactList.add(tempContact);
-						System.out.println("Contact: firstName = " + tempContact.get_firstName());
+							tempContactList.add(tempContact);
+							System.out.println("Contact: firstName = " + tempContact.get_firstName());
 						
-					} else {
+						} else {
 						Contact tempContact = new Contact(infoArr[0], infoArr[1], infoArr[2], infoArr[3], infoArr[4],
 								infoArr[5], infoArr[6], infoArr[7], infoArr[8], infoArr[9]);
 						tempContactList.add(tempContact);
 						System.out.println("Contact: firstName = " + tempContact.get_firstName());
-					}
+						}
 					/*
 					 * System.out.println("Contact : firstName= " + contact[0] +
 					 * " , lastName=" + contact[1] + " , street address=" +
 					 * contact[2] + " , city=" + contact[3] + " , state=" +
 					 * contact[4] + " , email=" + contact[5] ); //For Debugging
 					 */
+					}
 
 				
 			}
@@ -53,5 +57,25 @@ public class Reader {
 			// e.printStackTrace();
 		}
 		return tempContactList;
+	}
+	
+	public static boolean isValid(String[] contactInfo){
+		boolean firstNameEmpty = contactInfo[5].isEmpty();
+		boolean lastNameEmpty = contactInfo[6].isEmpty();
+		int otherFields = 0;
+		for(int i = 0; i < contactInfo.length; i++){
+			if(!contactInfo[i].isEmpty()){
+				otherFields += 1;
+			}
+		}
+		if(firstNameEmpty && lastNameEmpty){
+			return false;
+		}
+		else if((firstNameEmpty || lastNameEmpty) && (otherFields < 1)){
+			return false;
+		}
+		else{
+			return true;
+		}
 	}
 }
